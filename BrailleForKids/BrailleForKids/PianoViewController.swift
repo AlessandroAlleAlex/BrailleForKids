262b71e5a340b3d231a40a9814dcf07da3e50e57
Adila Abudureheman <aabudureheman@gmail.com>
2019-10-25 23:18:06 -0700

    PIANO INIT

---
+++ BrailleForKids/BrailleForKids/PianoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PianoViewController.swift
+//  BrailleForKids
+//
+//  Created by Adila on 10/25/19.
+//  Copyright © 2019 Alessandro Liu. All rights reserved.
+//
+
+import UIKit
+
+class PianoViewController: UIViewController {
+
+//    @IBOutlet weak var wtwo: UIButton!
+//    @IBOutlet weak var wOne: UIButton!
+//    @IBOutlet weak var wThree: UIButton!
+//    @IBOutlet weak var wSeven: UIButton!
+//    @IBOutlet weak var bThree: UIButton!
+//    @IBOutlet weak var bseven: UIButton!
+//    @IBOutlet weak var bsix: UIButton!
+//    @IBOutlet weak var bfive: UIButton!
+//    @IBOutlet weak var bone: UIButton!
+//    @IBOutlet weak var wEight: UIButton!
+//    @IBOutlet weak var wSix: UIButton!
+//
+//    @IBOutlet weak var wfive: UIButton!
+//    @IBOutlet weak var wFour: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
