7a74318c6ff050b0c00d5643caeade1bdc09e945
Adila Abudureheman <aabudureheman@gmail.com>
2019-10-26 15:20:24 -0700

    Adila push

--- BrailleForKids/BrailleForKids/PianoViewController.swift
+++ BrailleForKids/BrailleForKids/PianoViewController.swift
@@ -1,4 +1,4 @@
-//
+
 //  PianoViewController.swift
 //  BrailleForKids
 //
@@ -7,30 +7,102 @@
 //
 
 import UIKit
+import AVFoundation
+class PianoViewController: UIViewController, AVAudioPlayerDelegate {
 
-class PianoViewController: UIViewController {
-
-//    @IBOutlet weak var wtwo: UIButton!
-//    @IBOutlet weak var wOne: UIButton!
-//    @IBOutlet weak var wThree: UIButton!
-//    @IBOutlet weak var wSeven: UIButton!
-//    @IBOutlet weak var bThree: UIButton!
-//    @IBOutlet weak var bseven: UIButton!
-//    @IBOutlet weak var bsix: UIButton!
-//    @IBOutlet weak var bfive: UIButton!
-//    @IBOutlet weak var bone: UIButton!
-//    @IBOutlet weak var wEight: UIButton!
-//    @IBOutlet weak var wSix: UIButton!
-//
-//    @IBOutlet weak var wfive: UIButton!
-//    @IBOutlet weak var wFour: UIButton!
+    @IBOutlet weak var a: UIButton!
     
+    @IBOutlet weak var b: UIButton!
+    @IBOutlet weak var c: UIButton!
+    @IBOutlet weak var d: UIButton!
+    @IBOutlet weak var e: UIButton!
+    @IBOutlet weak var f: UIButton!
+    @IBOutlet weak var g: UIButton!
+    @IBOutlet weak var h: UIButton!
+    @IBOutlet weak var i: UIButton!
+    @IBOutlet weak var j: UIButton!
+    @IBOutlet weak var k: UIButton!
+    @IBOutlet weak var l: UIButton!
+    @IBOutlet weak var m: UIButton!
+    @IBOutlet weak var n: UIButton!
+    
+    
+    var audioPlayer : AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let soundURL = Bundle.main.path(forResource: "a3", ofType: "mp3"){
+            let url = NSURL.fileURL(withPath: soundURL)
+            do{
+                try audioPlayer = AVAudioPlayer(contentsOf: url as URL)
+                    }catch {
+                                 print("there was some error.")
+                    }
+        }
+    }
+    func vibrate() {
+        AudioServicesPlaySystemSoundWithCompletion(kSystemSoundID_Vibrate) {
+            //AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+        }
+    }
+    
+    @IBAction func c3A(_ sender: Any) {
+        // vibrate()
+        audioPlayer?.play()
+    }
+    
+    @IBAction func d3B(_ sender: Any) {
+         vibrate()
         
     }
     
-
-
+    
+    @IBAction func e4C(_ sender: Any) {
+         vibrate()
+        
+    }
+    @IBAction func f3D(_ sender: Any) {
+         vibrate()
+        
+    }
+    @IBAction func g3E(_ sender: Any) {
+         vibrate()
+        
+    }
+    @IBAction func a3F(_ sender: Any) {
+         vibrate()
+        
+    }
+    @IBAction func h3G(_ sender: Any) {
+         vibrate()
+    }
+        
+    
+    @IBAction func c4H(_ sender: Any) {
+        vibrate()
+    }
+    
+    @IBAction func cis3I(_ sender: Any) {
+        vibrate()
+    }
+    
+    @IBAction func dis4J(_ sender: Any) {
+         vibrate()
+    }
+    
+    @IBAction func fis3k(_ sender: Any) {
+         vibrate()
+    }
+    
+    @IBAction func gis3L(_ sender: Any) {
+         vibrate()
+    }
+    
+    @IBAction func As3M(_ sender: Any) {
+         vibrate()
+    }
+    
+    @IBAction func cis4N(_ sender: Any) {
+         vibrate()
+    }
 }
+
