4762ccc99a139efbb454de034acdc9ce2a01f49f
AL <alessandro@campus-039-037.ucdavis.edu>
2020-01-18 14:22:12 -0800

    add a view

---
+++ BrailleForKids/BrailleForKids/SpeechRegVC.swift
@@ -0,0 +1,30 @@
+//
+//  SpeechRegVC.swift
+//  BrailleForKids
+//
+//  Created by AL on 1/18/20.
+//  Copyright © 2020 Alessandro Liu. All rights reserved.
+//
+
+import UIKit
+
+class SpeechRegVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
