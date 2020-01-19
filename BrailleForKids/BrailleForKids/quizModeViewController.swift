78e789aeb08d4b428a4252c94a045a115674e25b
aadila6 <aabudureheman@gmail.com>
2020-01-19 01:49:04 -0800

    Update UI & others

---
+++ BrailleForKids/BrailleForKids/quizModeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  quizModeViewController.swift
+//  BrailleForKids
+//
+//  Created by Adila on 1/19/20.
+//  Copyright © 2020 Alessandro Liu. All rights reserved.
+//
+
+import UIKit
+
+class quizModeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
